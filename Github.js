4e8fa14211f27be07ea0a119e7f5ad4682ed325a
Ahamed Safnaj <safnaj99@gmail.com>
2019-07-20 16:58:09 +0530

    Github.js

--- Github.js
+++ Github.js
@@ -16,8 +16,8 @@ class Github{
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
             
-        const profile = await profileResponse.json();
-        const repos = await repoResponse.json();
+        const profile = await profileResponse.json(); //Storing Profile Response
+        const repos = await repoResponse.json();      //Storing Repo Response
     
         return {
           profile,
