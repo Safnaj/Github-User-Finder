451414db54c9dd2ea5763a9919b1f2d53fda04d7
Safnaj <safnaj99@gmail.com>
2021-09-01 00:28:55 +0530

    Headers Added

--- Github.js
+++ Github.js
@@ -1,28 +1,43 @@
-class Github{
-    
-/**
- * Author Safnaj on 03/07/2019
- * Project Github-User-Finder
- **/
+class Github {
+  /**
+   * Author Safnaj on 03/07/2019
+   * Project Github-User-Finder
+   **/
 
-    constructor(){
-        this.client_id = '8333fc3680f2476f6df5';
-        this.client_secret = '233e425ed6e3e58bc61c608c17015266aefd91ad';
-        this.repos_count = 5;
-        this.repos_sort = 'created : asc';
-    }
+  constructor() {
+    this.client_id = "8333fc3680f2476f6df5";
+    this.client_secret = "233e425ed6e3e58bc61c608c17015266aefd91ad";
+    this.repos_count = 5;
+    this.repos_sort = "created : asc";
+  }
 
-    async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
-            
-        const profile = await profileResponse.json(); //Storing Profile Response
-        const repos = await repoResponse.json();      //Storing Repo Response
-    
-        //returns
-        return {
-          profile,
-          repos
-        }
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}`,
+      {
+        headers: {
+          client_id: this.client_id,
+          client_secret: this.client_secret,
+        },
       }
+    );
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
+      {
+        headers: {
+          client_id: this.client_id,
+          client_secret: this.client_secret,
+        },
+      }
+    );
+
+    const profile = await profileResponse.json(); //Storing Profile Response
+    const repos = await repoResponse.json(); //Storing Repo Response
+
+    //returns
+    return {
+      profile,
+      repos,
+    };
+  }
 }
